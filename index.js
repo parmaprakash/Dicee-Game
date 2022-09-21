161edd470620182818fb8fd2a0cab0b9ed95f251
Prakash Dhayal <pdcoding19@gmail.com>
2022-09-21 11:25:06 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function refreshMe() {
+  var num1 = Math.floor(Math.random() * 6 + 1);
+  var num2 = Math.floor(Math.random() * 6 + 1);
+  document
+    .querySelector(".img1")
+    .setAttribute("src", "images/dice" + num1 + ".png");
+  document
+    .querySelector(".img2")
+    .setAttribute("src", "images/dice" + num2 + ".png");
+  if (num1 > num2) {
+    document.querySelector("h1").textContent = "😂 Player 1 Wins";
+  } else if (num1 < num2) {
+    document.querySelector("h1").textContent = "Player 2 Wins 😂";
+  } else {
+    document.querySelector("h1").textContent = "Draw !";
+  }
+}
